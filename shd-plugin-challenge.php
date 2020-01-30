f04126f1dfc227f238c80504b54d52939b3a774c
Tyler Hueter <tylerhueter08@gmail.com>
2020-01-29 23:33:49 -0500

    initial commit

---
+++ shd-plugin-challenge.php
@@ -0,0 +1,28 @@
+<?php 
+
+/*
+Plugin Name: SHD Plugin Challenge
+Plugin URI:  http://link to your plugin homepage
+Description: This plugin was built to complete a code challenge.
+Version:     1.0
+Author:      Tyler Hueter
+Author URI:  http://link to your website
+License:     GPL2 etc
+License URI: https://link to your plugin license
+
+Copyright 2020 SHD Plugin Challenge (email : tylerhueter08@gmail.com)
+SHD Plugin Challenge is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+SHD Plugin Challenge is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with SHD Plugin Challenge. If not, see (http://link to your plugin license).
+*/
+
+?>
